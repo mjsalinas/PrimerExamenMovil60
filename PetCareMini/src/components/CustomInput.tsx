97e36e27b981ddaae496529b0ded276afdb3c833
acano2791 <acano2791@gmail.com>
2026-02-20 22:41:57 -0600

    - Se realizo un cambio para corregir el icono para mostrar u ocultar la contraseña

--- PetCareMini/src/components/CustomInput.tsx
+++ PetCareMini/src/components/CustomInput.tsx
@@ -64,7 +64,7 @@ export default function CustomInput({
             style={styles.eyeButton}
           >
             <Ionicons
-              name={showPassword ? 'eye-outline' : 'eye-outline'}
+              name={showPassword ? 'eye-outline' : 'eye-off'}
               size={22}
               color="#607D8B"
             />
