8838cde95c5e06213fdd206e1c3551a2167804d7
acano2791 <acano2791@gmail.com>
2026-02-20 22:28:30 -0600

    - se corrige el estilo en CustomButtonm, agregando buttonStyle - se creo disabledText para solucionar error en la linea 47 en CustomButton

--- PetCareMini/src/components/CustomButton.tsx
+++ PetCareMini/src/components/CustomButton.tsx
@@ -39,7 +39,7 @@ export default function CustomButton({
 
   return (
     <TouchableOpacity
-      style={[styles.base]}
+      style={[styles.base , buttonStyle]}
       onPress={onPress}
       disabled={disabled}
       activeOpacity={0.7}
@@ -79,5 +79,7 @@ const styles = StyleSheet.create({
   secondaryText: {
     color: '#4A90D9',
   },
-  
+   disabledText: {
+    color: '#d94a5d',
+  },
 });
